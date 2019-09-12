9d1a7e291fecc325acfc29013d389d08c7a4d4ec
hitourebito <yuto0423777@gmail.com>
2019-09-12 14:45:57 +0900

    Copy base to ex07_03.php

---
+++ ex07_03.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="Pragma" content="no-cache">
+  <meta http-equiv="Cache-Control" content="no-store">
+  <meta http-equiv="Expires" content = "0">
+  <title>ex07_03.php</title>
+</head>
+<body>
+<?php //phpここから
+
+//phpここまで ?>
+</body>
