4c03736a749ddb040b4f9bb7593fdfb0b58f9a18
hitourebito <yuto0423777@gmail.com>
2019-09-12 14:00:22 +0900

    add form checkbox

--- ex07_04.php
+++ ex07_04.php
@@ -11,11 +11,21 @@
 <?php //phpここから
   if ($_SERVER["REQUEST_METHOD"] == "POST") 
   {
-    
   } else {
 //phpここまで ?>
-
-  <?php } 
+  <form action="<?= $_SERVER["SCRIPT_NAME"]?>" method="POST">
+    <div>時刻を表示したい地域:<br/> 
+      <input type="checkbox" name="require_value[]" value="イースター島">イースター島<br/>
+      <input type="checkbox" name="require_value[]" value="モルディブ">モルディブ<br/>
+      <input type="checkbox" name="require_value[]" value="ローマ">ローマ<br/>
+      <input type="checkbox" name="require_value[]" value="モスクワ">モスクワ<br/>
+      <input type="checkbox" name="require_value[]" value="シドニー">シドニー<br/>
+      <input type="checkbox" name="require_value[]" value="シンガポール">シンガポール<br/>
+      <input type="checkbox" name="require_value[]" value="平壌">平壌<br/><br/>
+    </div>
+    <input type="submit" name="btn" value="送信">
+  </form>
+<?php } 
   
-  ?>
+?>
 </body>
