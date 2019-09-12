b257823e7e24eeedbf7fbf5a0677519d1c7bdd28
hitourebito <yuto0423777@gmail.com>
2019-09-12 13:48:26 +0900

    Copy base to ex07_04.php

---
+++ ex07_04.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="Pragma" content="no-cache">
+  <meta http-equiv="Cache-Control" content="no-store">
+  <meta http-equiv="Expires" content = "0">
+  <title>ex07_04.php</title>
+</head>
+<body>
+<?php //phpここから
+  if ($_SERVER["REQUEST_METHOD"] == "POST") 
+  {
+    
+  } else {
+//phpここまで ?>
+
+  <?php } 
+  
+  ?>
+</body>
